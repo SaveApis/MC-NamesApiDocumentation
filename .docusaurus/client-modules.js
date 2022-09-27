ba01e4a7996858c518aea797c6917539876c1b82
SaveFeelix <felixhake@web.de>
2022-09-27 15:48:08 +0200

    Remove unimportant Files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('E:\\Privat\\SaveApis\\MC-NamesApiDocumentation\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('E:\\Privat\\SaveApis\\MC-NamesApiDocumentation\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('E:\\Privat\\SaveApis\\MC-NamesApiDocumentation\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('E:\\Privat\\SaveApis\\MC-NamesApiDocumentation\\src\\css\\custom.css'),
-];
