d946a1044e81b367963143e153992536c9636708
SaveFeelix <felixhake@web.de>
2022-09-28 15:13:58 +0200

    ==================== Blog ==================== Disable Blog in docusaurus.config.js Remove blog Folder ==============================================

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,6 +46,7 @@ const config = {
                     editUrl:
                         'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
                 },
+                blog: false,
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
                 },
