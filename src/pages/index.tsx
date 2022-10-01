97eb1326493ad113a13fab5899c5a21b9dcf4f67
SaveFeelix <felixhake@web.de>
2022-10-01 14:18:45 +0200

    ==================== Translations ==================== Add Translations > code.json Add Items to translate > index.tsx ======================================================

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
+import Translate from "@docusaurus/Translate";
 
 function HomepageHeader() {
     const {siteConfig} = useDocusaurusContext();
@@ -14,10 +15,16 @@ function HomepageHeader() {
             <div className="container">
                 <h1 className="hero__title">{siteConfig.title}</h1>
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
-                <p>
-                    API um die Relation zwischen UUID, Minecraft Name und der Name History zu ermitteln.<br/>
+                <Translate id="title.description" description="The Project Description">
+                    Diese Projekt stellt eine API bereit um die Relation zwischen UUID, Minecraft Name und der Name
+                    History zu ermitteln.
+                </Translate>
+                <br/>
+                <Translate id="title.include" description="Included components in Project">
                     Enthalten sind eine REST-API, eine Java Library und eine C# Library.
-                </p>
+                </Translate>
+                <br/>
+                <br/>
                 <div className={styles.buttons}>
                     <Link
                         className="button button--secondary button--lg"
